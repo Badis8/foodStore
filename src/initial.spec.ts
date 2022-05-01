3104bbee22ebc0447253beff513333955c4ea0ba
DESKTOP-8QHVFT5\Badis Ben Sassi <badisbensassi8@gmail.com>
2022-05-01 20:26:16 +0100

    first service :storing users

--- src/initial.spec.ts
+++ src/initial.spec.ts
@@ -1,7 +1,24 @@
-export interface user{
+export interface User{
     id:string,
     pseudoName:string,
 }
+export class UserStore{
+    constructor(private readonly users:Array<User>){
+    }
+    store(user:User){
+        this.users.push(user);
+    }
+    getUsers(){
+        return this.users
+    }
+}
 test('should store a user',()=>{
-    
+    const userStore=new UserStore([]); 
+    const user:User=
+    {
+        id:'123,',
+        pseudoName:'badis',
+    }
+    userStore.store(user)
+    expect(userStore.getUsers()[0]).toBe(user);
 });
