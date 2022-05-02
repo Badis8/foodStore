07cf7ad7609903a70c2d148532af15979e94218c
DESKTOP-8QHVFT5\Badis Ben Sassi <badisbensassi8@gmail.com>
2022-05-02 18:55:49 +0100

    adding user selectors

--- src/initial.spec.ts
+++ src/initial.spec.ts
@@ -1,6 +1,7 @@
 export interface User{
     id:string,
     pseudoName:string,
+    password:string,
 }
 export class UserStore{
     constructor(private readonly users:Array<User>){
@@ -11,14 +12,58 @@ export class UserStore{
     getUsers(){
         return this.users
     }
+    getUser(id:string){
+        for (const user of this.users){
+            if(user.id===id){
+                return user;
+            }
+        }
+    }
+    getIdBypseudoName(pseudoName:string):string{
+        for (const user of this.users){
+            if(user.pseudoName===pseudoName){
+                return user.id;
+            }
+        }
+        return 'unfound'
+    }
 }
+describe('testing user connection ',()=>{
 test('should store a user',()=>{
     const userStore=new UserStore([]); 
     const user:User=
     {
         id:'123,',
         pseudoName:'badis',
+        password:'321',
     }
-    userStore.store(user)
+
+    userStore.store(user);
+
     expect(userStore.getUsers()[0]).toBe(user);
+});
+test('should get user by id',()=>{
+    const userStore=new UserStore([]); 
+    const user:User=
+    {
+        id:'123',
+        pseudoName:'badis',
+        password:'321',
+    }      
+    userStore.store(user);
+    const fetchedUser=userStore.getUser('123');
+    expect(fetchedUser).toBe(user);
+});
+test('should get id by name and password',()=>{
+    const userStore=new UserStore([]); 
+    const user:User=
+    {
+        id:'123',
+        pseudoName:'badis',
+        password:'321',
+    }      
+    userStore.store(user);
+    const id=userStore.getIdBypseudoName('badis');
+    expect(id).toBe(user.id);
+})
 });
